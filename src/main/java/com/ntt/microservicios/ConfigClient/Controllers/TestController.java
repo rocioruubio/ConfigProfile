6745c860854ff635107cc58c69f34279f48b74c0
Rocío <rocioorubio17@gmail.com>
2021-11-15 14:32:12 +0100

    segunda subida con los profiles

--- src/main/java/com/ntt/microservicios/ConfigClient/Controllers/TestController.java
+++ src/main/java/com/ntt/microservicios/ConfigClient/Controllers/TestController.java
@@ -9,13 +9,21 @@ public class TestController {
 	
 	@Value("${some.value}")
 	private String myValue;
+
+	@Value("${spring.profiles.active}")
+	private String activeProfiles;
 	
 	@GetMapping(path="/myvalue")
 	public String myValue() {
+		for (String profileName : activeProfiles.split(",")) {
+            if(profileName.equals("dev")) {
+            	this.myValue = "Hola soy rocio";
+            }else if(profileName.equals("prod")) {
+            	this.myValue="Soy tester";
+            }
+        }
 		return this.myValue;
 	}
-	
-	
-	
+
 	
 }
