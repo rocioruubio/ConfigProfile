f277543330fe4079ceb2148b23d68b9a85c42e4a
Rocío <rocioorubio17@gmail.com>
2021-11-15 13:15:29 +0100

    primera subida

---
+++ src/main/java/com/ntt/microservicios/ConfigClient/Controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.ntt.microservicios.ConfigClient.Controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@Value("${some.value}")
+	private String myValue;
+	
+	@GetMapping(path="/myvalue")
+	public String myValue() {
+		return this.myValue;
+	}
+	
+	
+	
+	
+}
